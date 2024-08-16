4f91f370cc4b68350d2b53b10a4756ad88b418f9
Alexander Hamilton <a.hamilton72006@gmail.com>
2024-08-16 13:22:55 -0700

    Working reload command

--- src/register.js
+++ src/register.js
@@ -7,6 +7,7 @@ const commands = [];
 // Grab all the command folders from the commands directory you created earlier
 const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
+let reloadCommand;
 
 for (const folder of commandFolders) {
 	// Grab all the command files from the commands directory you created earlier
@@ -17,7 +18,11 @@ for (const folder of commandFolders) {
 		const filePath = path.join(commandsPath, file);
 		const command = require(filePath);
 		if ('data' in command && 'execute' in command) {
-			commands.push(command.data.toJSON());
+			if (command.name === 'reload') {
+				reloadCommand = command.data.toJSON();
+			} else {
+				commands.push(command.data.toJSON());
+			}
 		} else {
 			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
 		}
@@ -37,6 +42,15 @@ const rest = new REST().setToken(token);
 			Routes.applicationGuildCommands(clientId, guildId),
 			{ body: commands },
 		);
+
+		// If reloadCommand is defined, deploy the reload command separately
+		if (reloadCommand) {
+			await rest.put(
+				// GuildID is for "sdfrc bot we so back" test server
+				Routes.applicationGuildCommand(clientId, 1270642179592814592, reloadCommand.id),
+				{ body: reloadCommand },
+			);
+		}
 
 		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
 	} catch (error) {
