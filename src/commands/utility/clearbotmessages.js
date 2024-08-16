c4958925574e4f1c27ff550478daf7b372206b9b
Alexander Hamilton <a.hamilton72006@gmail.com>
2024-08-16 14:30:45 -0700

    Make clearBotMessages accept a param for minsAgo

--- src/commands/utility/clearbotmessages.js
+++ src/commands/utility/clearbotmessages.js
@@ -1,18 +1,24 @@
 const { SlashCommandBuilder, PermissionsBitField } = require('discord.js');
 
 module.exports = {
+	category: 'utility',
 	data: new SlashCommandBuilder()
 		.setName('clearbotmessages')
 		.setDescription('Clears all messages from the bot sent within the past 5 minutes.')
-		.setDefaultMemberPermissions(PermissionsBitField.Flags.ManageMessages),
+		.addIntegerOption(option =>
+			option.setName('minutes_ago')
+				.setDescription('The number of minutes to look back for bot messages.')
+				.setRequired(false))
+		.setDefaultMemberPermissions(PermissionsBitField.Flags.Administrator),
 	async execute(interaction) {
+		const minutesAgo = interaction.options.getInteger('minutes_ago') || 5;
 		const channel = interaction.channel;
 		const botId = interaction.client.user.id;
-		const fiveMinutesAgo = Date.now() - 5 * 60 * 1000;
+		const timestampAgo = Date.now() - minutesAgo * 60 * 1000;
 
 		try {
 			const messages = await channel.messages.fetch({ limit: 100 });
-			const botMessages = messages.filter(msg => msg.author.id === botId && msg.createdTimestamp >= fiveMinutesAgo);
+			const botMessages = messages.filter(msg => msg.author.id === botId && msg.createdTimestamp >= timestampAgo);
 
 			await channel.bulkDelete(botMessages);
 
