91a480a9aaa7adda6eaae070ad4d6c96f28545a2
Alexander Hamilton <a.hamilton72006@gmail.com>
2024-08-16 13:46:04 -0700

    Make the reload command reload all commands if none were specified

--- src/commands/utility/reload.js
+++ src/commands/utility/reload.js
@@ -1,5 +1,18 @@
 const { SlashCommandBuilder, PermissionsBitField } = require('discord.js');
 
+function attemptCommandReload(command, commands) {
+	delete require.cache[require.resolve(`../${command.category}/${command.data.name}.js`)];
+	try {
+		commands.delete(command.data.name);
+		const newCommand = require(`../${command.category}/${command.data.name}.js`);
+		commands.set(newCommand.data.name, newCommand);
+		return `Command \`${newCommand.data.name}\` was reloaded!\n`;
+	} catch (error) {
+		console.error(error);
+		return `There was an error while reloading command \`${command.data.name}\`:\n\`${error.message}\``;
+	}
+}
+
 module.exports = {
 	category: 'utility',
 	data: new SlashCommandBuilder()
@@ -8,26 +21,29 @@ module.exports = {
 		.addStringOption(option =>
 			option.setName('command')
 				.setDescription('The command to reload.')
-				.setRequired(true))
+				.setRequired(false))
         .setDefaultMemberPermissions(PermissionsBitField.Flags.Administrator),
 	async execute(interaction) {
-		const commandName = interaction.options.getString('command', true).toLowerCase();
-		const command = interaction.client.commands.get(commandName);
+		const commandName = interaction.options.getString('command')?.toLowerCase();
+		const commands = interaction.client.commands;
 
-		if (!command) {
-			return interaction.reply(`There is no command with name \`${commandName}\`!`);
-		}
+		if (!commandName) {
+			await interaction.reply(`Reloading Commands...`);
+			let message = '';
+			
+			const oldCommands = commands.clone();
+			oldCommands.forEach(async (command) => {
+				message += attemptCommandReload(command, commands);
+				await interaction.editReply(message);
+			});
+		} else {
+			const command = commands.get(commandName);
 
-		delete require.cache[require.resolve(`../${command.category}/${command.data.name}.js`)];
+			if (!command) {
+				return interaction.reply(`There is no command with name \`${commandName}\`!`);
+			}
 
-		try {
-	        interaction.client.commands.delete(command.data.name);
-	        const newCommand = require(`../${command.category}/${command.data.name}.js`);
-	        interaction.client.commands.set(newCommand.data.name, newCommand);
-	        await interaction.reply(`Command \`${newCommand.data.name}\` was reloaded!`);
-		} catch (error) {
-	        console.error(error);
-	        await interaction.reply(`There was an error while reloading a command \`${command.data.name}\`:\n\`${error.message}\``);
+			await interaction.reply(attemptCommandReload(command, commands));
 		}
 	},
 };
