f25f2c50a626230aba75a03a32e7ae4bbfeb1c68
Alexander Hamilton <a.hamilton72006@gmail.com>
2024-08-20 02:04:37 -0700

    Modify regex to allow all non alphanumeric characters wrapping words, and set chances to 1/7

--- src/events/funStuff.js
+++ src/events/funStuff.js
@@ -1,10 +1,5 @@
 const { Events } = require("discord.js");
 
-const replyMap = {
-    "you know what that means": "https://cdn.discordapp.com/attachments/851596893678731267/1231435106242269285/YouKnowWhatThatMeansFRC.gif",
-    "yippee": "https://media.discordapp.net/attachments/1228490698932752454/1266526296549363755/Yippee1.gif"
-};
-
 const reactionMap = {
     "susan": "💻🐈",
     "cat": "🐈",
@@ -15,42 +10,23 @@ const reactionMap = {
     "issue": "☹",
     "254": "🧀💨",
     "1622": "🕷",
-    "359": "🏄‍♂️",
+    "359": "🏄",
     "🤓": "☝🤓",
     "ackshually": "☝🤓",
 };
-
-const REPLY_COOLDOWN = 120_000;
-const REACT_COOLDOWN = 30_000;
-let lastReply = 0;
-let lastReaction = 0;
 
 module.exports = {
     name: Events.MessageCreate,
     async execute(message) {
         if (message.author.bot) return;
 
-        const now = Date.now();
-        for (const [keyword, url] of Object.entries(replyMap)) {
-            if (message.content.toLowerCase().includes(keyword.toLowerCase())) {
-                if (now - lastReply > REPLY_COOLDOWN && Math.random() < 0.34) {
-                    lastReply = now;
-                    setTimeout(async () => {
-                        await message.reply(url);
-                    }, Math.random() * 3000);
-                    break;
-                }
-            }
-        }
         for (const [keyword, reaction] of Object.entries(reactionMap)) {
-            if (message.content.toLowerCase().includes(keyword.toLowerCase())) {
-                if (now - lastReaction > REACT_COOLDOWN || Math.random() < 0.7) {
-                    lastReaction = now;
-                    setTimeout(async () => {
-                        for (const emoji of reaction) {
-                            await message.react(emoji);
-                        }
-                    }, Math.random() * 3000);
+            const regex = new RegExp(`(?:^|[^a-zA-Z0-9])${keyword}(?:$|[^a-zA-Z0-9])`, 'i');
+            if (regex.test(message.content)) {
+                if (Math.random() < 0.15) {
+                    for (const emoji of reaction) {
+                        await message.react(emoji);
+                    }
                 }
             }
         }
