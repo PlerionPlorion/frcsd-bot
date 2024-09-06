26b195b50ab21cf4c06c8c290c57b7bf73afa9e0
Alexander Hamilton <a.hamilton72006@gmail.com>
2024-09-06 15:50:37 -0700

    Code cleanup, ana add an attempt for a url but it doenst work unfortunately. Doesn't hurt to have??

--- src/events/ready.js
+++ src/events/ready.js
@@ -12,64 +12,52 @@ module.exports = {
         let intervalId;
         let rolesList = [];
         // const guildID = '1231713772402643035' // test3
-        const guildID = '942815054271770654' // SDFRC
+        const guildID = "942815054271770654"; // SDFRC (frcsd >>>)
 
         const guild = client.guilds.cache.get(guildID);
         if (!guild) {
             console.error(`Guild with ID ${guildID} found.`);
             return;
         }
-            try {
-                // Iterate over each role in the guild
-                for (const role of guild.roles.cache.values()) {
-                    if (role.name.includes(" | ")) {
-                        const [roleId] = role.name.split(" | ");
-                        rolesList.push(roleId);
-                    }
+        try {
+            // Iterate over each role in the guild
+            for (const role of guild.roles.cache.values()) {
+                if (role.name.includes(" | ")) {
+                    const [roleId] = role.name.split(" | ");
+                    rolesList.push(roleId);
                 }
-            } catch (error) {
-                console.error(`Failed to process guild ${guild.id}: `, error);
             }
-        
+        } catch (error) {
+            console.error(`Failed to process guild ${guild.id}: `, error);
+        }
 
         if (rolesList.length > 0) {
             // Set presence before 6 hour loop
-            client.user.setPresence({
-                activities: [
-                    {
-                        name:
-                            "against " +
-                            rolesList[
-                                Math.floor(Math.random() * rolesList.length)
-                            ],
-                        type: 0,
-                        status: "online",
-                        afk: false,
-                    },
-                ],
-            });
-            intervalId = setInterval(() => {
+            function refreshPresence() {
+                const randomTeamNumber =
+                    rolesList[Math.floor(Math.random() * rolesList.length)];
                 client.user.setPresence({
                     activities: [
                         {
-                            name:
-                                "against " +
-                                rolesList[
-                                    Math.floor(Math.random() * rolesList.length)
-                                ],
+                            name: "against " + randomTeamNumber,
                             type: 0,
                             status: "online",
                             afk: false,
+                            url: `https://thebluealliance.com/team/${randomTeamNumber}`,
                         },
                     ],
                 });
-            }, 21600000);
+            }
+            // run once before the interval
+            refreshPresence();
+            intervalId = setInterval(() => {
+                refreshPresence();
+            }, 21600000); // 6 hours
             // Reset timer on bot shutdown
             process.on("SIGINT", () => {
                 clearInterval(intervalId);
                 process.exit(0);
             });
         }
-        // console.log("buh");
     },
 };
