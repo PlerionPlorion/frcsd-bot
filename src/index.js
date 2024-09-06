66976c755c63a0ec73433b8a1e8ac2f2e77097ee
Alexander Hamilton <a.hamilton72006@gmail.com>
2024-09-06 16:32:40 -0700

    Add debug and warn info to the client

--- src/index.js
+++ src/index.js
@@ -11,7 +11,8 @@ const client = new Client({
         GatewayIntentBits.MessageContent,
 		GatewayIntentBits.GuildMembers,
     ],
-});
+}).on("debug", console.log).on("warn", console.log);
+
 client.commands = new Collection();
 client.cooldowns = new Collection();
 
