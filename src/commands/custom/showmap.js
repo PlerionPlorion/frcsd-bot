8326ba5a1709e4ba4df21ef0afcc622b174ec57f
Plerion <41887002+PlerionPlorion@users.noreply.github.com>
2024-09-14 11:39:54 -0700

    formatting

--- src/commands/custom/showmap.js
+++ src/commands/custom/showmap.js
@@ -1,15 +1,22 @@
-const { SlashCommandBuilder, PermissionsBitField } = require('discord.js');
+const { SlashCommandBuilder, PermissionsBitField } = require("discord.js");
 const { loadReactionMap } = require("../../utils/reactionutils");
-
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('showmap') // This is the slash command name
-        .setDescription('Display the current reaction map'), // Command description
-    async execute(interaction) { // This is the function executed when the command is used
+        .setName("showmap") // This is the slash command name
+        .setDescription("Display the current reaction map"), // Command description
+    async execute(interaction) {
+        // This is the function executed when the command is used
         // Check if the user has administrator permissions
-        if (!interaction.member.permissions.has(PermissionsBitField.Flags.Administrator)) {
-            return interaction.reply({ content: "You do not have permission to use this command.", ephemeral: true });
+        if (
+            !interaction.member.permissions.has(
+                PermissionsBitField.Flags.Administrator
+            )
+        ) {
+            return interaction.reply({
+                content: "You do not have permission to use this command.",
+                ephemeral: true,
+            });
         }
 
         // Convert the reaction map object to a readable JSON string
@@ -17,5 +24,5 @@ module.exports = {
         const mapString = JSON.stringify(reactionMap, null, 2);
         // Reply to the interaction with the reaction map
         await interaction.reply(`\`\`\`json\n${mapString}\n\`\`\``);
-    }
+    },
 };
